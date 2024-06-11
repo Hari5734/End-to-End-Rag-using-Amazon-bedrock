5713ec4d746bf2e9be14b499a080866ad6bebb6d
Harivijay <k.harivijay@gmail.com>
2024-06-11 16:06:27 +0530

    Code updated

--- main.py
+++ main.py
@@ -8,9 +8,20 @@ from langchain.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 
+#Bedrock client
+bedrock = boto3.client(service_name = "bedrock-runtime", region_name = "us-east-1")
+
+#Get embeddings model from bedrock
+bedrock_embedding = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client= bedrock)
 
 
 def get_document():
     loader = PyPDFDirectoryLoader('data')
     documents = loader.load
+    text_splitter = RecursiveCharacterTextSplitter(
+                                               chunk_size =1000 , 
+                                               chunk_overlap =500)
+    docs = text_splitter.split_documents(documents)
+    return docs
+    # len(docs)
 
