31265b9161da6dd919d71b031a39d94d99efede8
Harivijay <k.harivijay@gmail.com>
2024-06-11 15:13:06 +0530

    Code updated

---
+++ main.py
@@ -0,0 +1,11 @@
+import boto3
+import streamlit as st
+from langchain.llms.bedrock import Bedrock
+from langchain.embeddings import BedrockEmbeddings
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+print('OK')
